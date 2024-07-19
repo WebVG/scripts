2384d7cbb04343c027a1cfff71c262f8cd7bcd22
Garrett <GVWebster.91@gmail.com>
2024-07-19 03:57:34 -0400

    Update Get-Hashr.ps1

--- Get-Hashr.ps1
+++ Get-Hashr.ps1
@@ -1,13 +1,1 @@
-CD Downloads
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-install-script Get-WindowsAutopilotinfo -Force
-
-Get-WindowsAutoPilotInfo.ps1 -OutputFile "intunehash.csv"
-
-$filePath = "intunehash.csv"
-$fileContent = Get-Content -Path $filePath -Raw
-set FILE_PATH=%USERPROFILE%\Downloads\intunehash.csv
-curl -X POST -d "api_dev_key=fIM05J0hZe7YWUNgIZ0aDiUvoiaDnRda" -d "api_option=paste" --data-urlencode "api_paste_code@$filepath" https://pastebin.com/api/api_post.php
-
-
 
